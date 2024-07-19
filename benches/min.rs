e5798b00bcfa85402215b5b210062e666cbc325c
Saveliy Yusufov <sy2685@columbia.edu>
2024-07-18 21:55:02 -0400

    Restructure min benchmark

--- benches/min.rs
+++ benches/min.rs
@@ -1,47 +1,49 @@
 #![feature(portable_simd)]
 
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion, Throughput};
 use simd_itertools::{MinSimd, SIMD_LEN};
 use std::{
     fmt::Debug,
     simd::{prelude::SimdPartialEq, Mask, Simd, SimdElement},
-    time::Duration,
 };
 
-fn benchmark_contains<'a, T: 'static + Copy + PartialEq + Default + Debug>(
-    _c: &mut Criterion,
-    name: &str,
-    len: usize,
-) where
+fn benchmark_min<'a, T: 'static + Copy + PartialEq + Default + Debug>(c: &mut Criterion)
+where
     T: SimdElement + std::cmp::PartialEq + std::cmp::Ord,
     Simd<T, SIMD_LEN>: SimdPartialEq<Mask = Mask<T::Mask, SIMD_LEN>>,
 {
-    let v1 = black_box(vec![T::default(); len]);
+    let mut group = c.benchmark_group(format!("max-{}", std::any::type_name::<T>()));
+    let mut len = 1;
 
-    let mut group = Criterion::default()
-        .warm_up_time(Duration::from_secs(1))
-        .measurement_time(Duration::from_secs(1));
-    group.bench_function(&format!("SIMD min {} {}", name, len), |b| {
-        b.iter(|| black_box(black_box(&v1).iter().min_simd()))
-    });
-    group.bench_function(&format!("Scalar min {} {}", name, len), |b| {
-        b.iter(|| black_box(v1.iter().min()))
-    });
+    while len < (1 << 11) {
+        let v1 = vec![T::default(); len];
+
+        group.throughput(Throughput::Elements(len as u64));
+
+        group.bench_function(BenchmarkId::new("SIMD", len), |b| {
+            b.iter(|| black_box(v1.iter().min_simd()))
+        });
+        group.bench_function(BenchmarkId::new("Scalar", len), |b| {
+            b.iter(|| black_box(v1.iter().min()))
+        });
+
+        len *= 10;
+    }
+
+    group.finish();
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
-    for n in (0..200).map(|x| x * 10) {
-        benchmark_contains::<u8>(c, "u8", n);
-        benchmark_contains::<i8>(c, "i8", n);
-        benchmark_contains::<u16>(c, "u16", n);
-        benchmark_contains::<i16>(c, "i16", n);
-        benchmark_contains::<u32>(c, "u32", n);
-        benchmark_contains::<i32>(c, "i32", n);
-        benchmark_contains::<u64>(c, "u64", n);
-        benchmark_contains::<i64>(c, "i64", n);
-        benchmark_contains::<isize>(c, "isize", n);
-        benchmark_contains::<usize>(c, "usize", n);
-    }
+    benchmark_min::<u8>(c);
+    benchmark_min::<i8>(c);
+    benchmark_min::<u16>(c);
+    benchmark_min::<i16>(c);
+    benchmark_min::<u32>(c);
+    benchmark_min::<i32>(c);
+    benchmark_min::<u64>(c);
+    benchmark_min::<i64>(c);
+    benchmark_min::<isize>(c);
+    benchmark_min::<usize>(c);
 }
 criterion_group!(benches, criterion_benchmark);
 criterion_main!(benches);
